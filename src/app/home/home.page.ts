1ab074e1f4b67f39e8b55b2983823a678cf3513b
AdriVergara <adriveroca@gmail.com>
2021-07-02 13:06:52 +0200

    Info interesante sobre persistencia

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/shared/services/authentication.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public authService: AuthenticationService) {}
+  
+  ngOnInit(): void {
+    console.log("En home ngOnInit: " + this.authService.isLoggedIn);
+  }
 
   public RegisterClicked()
   {
--- src/app/list-map/tabs/tabs.page.ts
+++ src/app/list-map/tabs/tabs.page.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { AuthenticationService } from 'src/shared/services/authentication.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
-  constructor(private menu: MenuController) { }
+export class TabsPage implements OnInit{
+  constructor(private menu: MenuController,
+    public authService: AuthenticationService) { }
+
+  ngOnInit(): void {
+    console.log("after login (ngOnInit): " + this.authService.isLoggedIn);
+  }
+
+  ionViewWillEnter(){
+    console.log("after login (will enter): " + this.authService.isLoggedIn);
+  }
 
   ionViewDidEnter(){
     // cambia la propiedad disabled del tag html  <ion-menu> en app.component.html
     // <ion-menu contentId="main-content" type="overlay" disabled="true" menuId="main-content">
     this.menu.enable(true, 'main-content');
+    console.log("after login (did enter): " + this.authService.isLoggedIn);
+  }
+
+  ionViewWillLeave(){
+    console.log("after login (will leave): " + this.authService.isLoggedIn);
   }
 
   ionViewDidLeave(){
     this.menu.enable(false, 'main-content');
+    console.log("after login (did leave): " + this.authService.isLoggedIn);
   }
-
 
 }
