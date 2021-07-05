23dab44e8f77f2ba32e3a7652066d80487e26fa0
AdriVergara <adriveroca@gmail.com>
2021-07-05 17:10:45 +0200

    firebase environment settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDoulXA96Z9It3HyFIsslkusumS-QcQml4",
+    authDomain: "ionicfirebase-a7300.firebaseapp.com",
+    projectId: "ionicfirebase-a7300",
+    storageBucket: "ionicfirebase-a7300.appspot.com",
+    messagingSenderId: "149382680253",
+    appId: "1:149382680253:web:38ab9bb358f7def54471d4",
+    measurementId: "G-1P5NYT3FJB"
+  }
 };
 
 /*
